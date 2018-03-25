d7b1199780f57753a8504c5e9b5670931af8a5e6
Javier Perez Primary <javierperezprimary@javiers-macbook-pro.local>
2018-03-25 18:08:34 -0500

    initial commit

--- QuartsFunJavierP/ViewController.swift
+++ QuartsFunJavierP/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
-
+    @IBAction func changeShape(_ sender: UISegmentedControl) {
+        let shapeSelection = Shape(rawValue: UInt(sender.selectedSegmentIndex))
+        if let shape = shapeSelection {
+            let funView = view as! QuartzFunView;
+            funView.shape = shape;
+            colorControl.isHidden = shape == Shape.image
+        }
+        
+        
+    }
+    
+    @IBAction func changeCoor(_ sender: UISegmentedControl) {
+        let drawingColorSelection =
+            DrawingColor(rawValue: UInt(sender.selectedSegmentIndex))
+        if let drawingColor = drawingColorSelection {
+            let funView = view as! QuartzFunView;
+            switch drawingColor {
+            case .red:
+                funView.currentColor = UIColor.red
+                funView.useRandomColor = false
+                
+            case .blue:
+                funView.currentColor = UIColor.blue
+                funView.useRandomColor = false
+                
+            case .yellow:
+                funView.currentColor = UIColor.yellow
+                funView.useRandomColor = false
+                
+            case .green:
+                funView.currentColor = UIColor.green
+                funView.useRandomColor = false
+                
+            case .random:
+                funView.useRandomColor = true
+            }
+        }
+    }
 }
 
